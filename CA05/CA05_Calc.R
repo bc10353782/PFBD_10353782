ef8bfc7a97caa582c56999b60c2f1f25b4a01569
10353782 <10353782@mydbs,ie>
2017-05-14 10:11:01 +0100

    Update Rad and Degree Funct

--- CA05/CA05_Calc.R
+++ CA05/CA05_Calc.R
@@ -24,24 +24,26 @@ exponent <- function(x,y){
 cube <- function(x){
   return(x**3.0)
 }
-print (cube(4))
+#print (cube(4))
 #7
 square <- function(x){
   return(x*x)
 }
-#print square(3*3)
+print (square(3*x))
 #8
-#squareroot <- function(x){
-#  return(sqrt(x))  
-#}
+squareroot <- function(x){
+  return(sqrt(x))  
+}
 #9    
-#rad <- function(x){
-#  return(x**3.0)
+rad <- function(x){
+  return(x * 3.1415926536/180)
 }
 #10
-#cube <- function(x){
-#  return(x**3.0)      
+degree <- function(x){
+  return(x* 180/3.1415926536)      
 }
+
+
 # take input from the user
 print("Select operation.")
 print("1.Add")
@@ -51,29 +53,20 @@ print("4.Divide")
 print("5.Exponent")
 print("6.Cube")
 print("7.Square")
+print("8.Squareroot")
+print("9.Radian")
+print("10.Degree")
 
-selection = as.integer(readline(prompt="Enter choice 1 ,2, 3, 4, 5, 6, 7        :   "  ))
+selection = as.integer(readline(prompt="Enter choice 1 ,2, 3, 4, 5, 6, 7, 8, 9, 10         :   "  ))
 num1 = as.numeric(readline(prompt="Enter first number: "))
 if (selection <= 5) {
   num2 = as.numeric(readline(prompt="Enter second number: "))
 }  
-operator <- switch(selection,"+","-","*","/","Exp","Cube","Square")
-result <- switch(selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2), exponent(num1, num2), cube(num), square(num))
+operator <- switch(selection,"+","-","*","/","Exp","Cube","Square","Squareroot","Radian","Degree")
+result <- switch(selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2), 
+                 exponent(num1, num2), cube(num1), square(num1), squareroot(num1), rad(num1), deg(num1))
 if (selection <= 5) {
   print(paste(num1, operator, num2, "=", result))
 } else
   print(paste(num1, operator, "=", result))
 
-#  def cube(num1):
-#    return num1 ** 3.0
-#   Square function, takes input and squares num1
-#  def square(num1):
-#    return num1 ** float(num1)
-#   Square Root function, takes input and gives square root of num1
-#  def sqrt(num1):
-#    return round((num1 ** .5),2)
-#   Radian function, takes num1 and converts to radians
-#  def rad(num1):
-#    return round((num1 * 3.1415926536/180),2)
-#   Degree function, takes num1 and converts to degrees
-#  def deg(num1):
