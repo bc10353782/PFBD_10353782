e9a80d8e1e57c43295dc92e44beff5669120bf57
10353782 <10353782@mydbs,ie>
2017-05-29 10:57:48 +0100

    Small updates to main file

--- CA_04/BC_changes.py
+++ CA_04/BC_changes.py
@@ -1,13 +1,11 @@
-
+import csv
 # open the file - and read all of the lines.
 changes_file = 'changes_python.txt'
 # use strip to strip out spaces and trim the line.
 
-#my_file = open(changes_file, 'r')
-#data = my_file.readlines()
 
 data = [line.strip() for line in open(changes_file, 'r')]
-
+return data
 # print the number of lines read
 print(len(data))
 
